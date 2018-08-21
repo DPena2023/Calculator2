eab6b6eebcc07d1b435417c17cc7b6d8012c3337
David Pena <davidhome@Davids-MacBook-Pro.local>
2018-08-21 15:06:03 -0700

    added multiply

--- src/main/java/edu/gcu/bootcamp/java/stephen/williams/calculator2/Calculator.java
+++ src/main/java/edu/gcu/bootcamp/java/stephen/williams/calculator2/Calculator.java
@@ -16,7 +16,9 @@ public class Calculator {
 		
 //		int resultOfCalculation = add(num1, num2);
 		
-		int resultOfCalculation = subtract(num1, num2);
+	//	int resultOfCalculation = subtract(num1, num2);
+		
+		int resultOfCalculation = multiply(num1, num2);
 		
 		displayResult(resultOfCalculation);
 		
@@ -37,13 +39,19 @@ public class Calculator {
 	
 	private static void displayResult(int result) {
 		
-		System.out.println("The difference between the two numbers is " + result);
+		System.out.println("When multiplied the result is " + result);
 	}
 	
 	private static int subtract(int num1, int num2) {
 		
 		int difference = num1 - num2;
 		return difference;
+	}
+	
+	private static int multiply(int num1, int num2) {
+		
+		int multiply = num1 * num2;
+		return multiply;
 	}
 	
 }
